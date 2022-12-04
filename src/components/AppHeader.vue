13e075d9394757a4814fafc980e3998b7645a5cb
a.karamiani <aziz.karamiani@gmail.com>
2022-12-04 19:57:20 +0330

    Header Template

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>App Header</div>
+  <div class="ui secondary pointing menu">
+    <a class="active item">
+      Home
+    </a>
+    <a class="item">
+      Messages
+    </a>
+    <a class="item">
+      Friends
+    </a>
+    <div class="right menu">
+      <a class="ui item">
+        Logout
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "AppHeader"
+  name: "AppHeader"
 }
 </script>
 
