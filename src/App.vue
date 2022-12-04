71d39013667b2c760380ead94b6927aedf3a5017
a.karamiani <aziz.karamiani@gmail.com>
2022-12-04 19:46:06 +0330

    Add AppHeader component and semantic UI

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <div>
-    <span>It's Works.</span>
+    <AppHeader></AppHeader>
   </div>
 </template>
 
 <script>
 
+import AppHeader from "@/components/AppHeader.vue";
+
 export default {
   name: 'App',
   components: {
+    AppHeader,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
