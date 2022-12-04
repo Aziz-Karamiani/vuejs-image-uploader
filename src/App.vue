59bdbbbf7e967caaf52df795028d6a43f92872fa
a.karamiani <aziz.karamiani@gmail.com>
2022-12-04 19:31:09 +0330

    Remove Extra code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <span>It's Works.</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
